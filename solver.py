a44edfad893964c78894d5aaf43c3901ab628693
pstuder <pablo.studer@jalasoft.com>
2013-07-12 09:32:16 -0400

    Algorithm & Norvig [id=68096]
    Pablo: Meet PEP8 and PEP257, moved out data structures and methods from
    algorithm class, since they are not used by any other than norvig.
    Pablo: Fix broken code in unit tests. Added one more test to get 100%
    coverage.

--- solver.py
+++ solver.py
@@ -1,72 +1,66 @@
-from validmatrix import MatrixHandler
 from itertools import product
 
+from validmatrix import MatrixHandler
+
+
 class Algorithm:
-	
+	"""Common abstract class for sudoku algorithms."""
 	def __init__(self, matrixhandler):
-        #Instance to matrix handler used as input for any algorithm.
+		"""Initializes a new algorithm instance.
+		
+		The following instance attributes are created:
+		_input_matrix -- matrixhandler containing the input sudoku to be solved.
+		
+		The following instances for default structures used by any algorithm
+		are also created:
+		_digits -- '123456789'
+		_rows -- 'ABCDEFGHI'
+		_columns -- same as _digits
+
+		"""
 		self._input_matrix = matrixhandler
 		
-		# Default structures to be used by any algorithm:
-		# digits, rows and columns
 		self._digits = '123456789'
 		self._rows = 'ABCDEFGHI'
 		self._columns = self._digits
 		
-		# Default fields and values for any algorithm as:
-		# squares = ['A1', 'A2' ..., 'B1', 'B2', ... , 'I9']
-		# values = {'A1':'123456789', ... 'I9':'123456789'}
-		self._squares = self._cross(self._rows, self._columns)
-		self._values = dict((s, self._columns) for s in self._squares)
-		
-		# Don't use this. It is a constructor for "self._units". See next.
-		self._unitlist = ([self._cross(self._rows, c) for c in self._columns] +
-			[self._cross(r, self._columns) for r in self._rows] +
-			[self._cross(rs, cs) for rs in ('ABC','DEF','GHI')
-			for cs in ('123','456','789')])
-		
-		# Field influenced by rows, columns and quadrants
-		# will be saved in two types of dictionaries:
-		# units = {'A1': [[row], [column], [quadrant]], ... }
-		# peers = {'A1': set(row, column, quadrant), ... }
-		self._units = dict((s, [u for u in self._unitlist if s in u])
-			for s in self._squares)
-		self._peers = dict((s, set(sum(self._units[s], [])) - set([s]))
-			for s in self._squares)
-		
-	def solve(self, algorithm_type):
-		"""
-		solve(algorithm_type) -> matrixhandler
-		
-		Try to solve the input matrixhandler using algorithm_type.
-		Return output matrixhandler with the solution or return None,
-		If sudoku has no solution.
-		"""
-		raise ImplementationError("Not yet implemented!")
+	def solve(self):
+		"""Raises NotImplementedError, since this is an absract class."""
+		raise NotImplementedError("Can't solve if not a specific algorithm!")
 
 	def _cross(self, A, B):
-		"""
-		_cross(A, B) -> List
-		
-		Cross elements of A sequence with each element in B sequence and return
-		a list with all combinations.
-		"""
+		"""Returns cross elements in A with each element in B"""
 		return [a + b for a in A for b in B]
 	
 	def _some(self, sequence):
-		"""
-		_some(sequence) -> element
-		
-		Return first element of seq that is true.
+		"""Return first element of seq that is true.
+
 		Useful for quick pick-ups and verifications for elements != '' or 0.
 		If all elements of the sequence are '' or 0, return False.
+
 		"""
 		for element in sequence:
 			if element: return element
 		return False
 
+
 class Norvig(Algorithm):
-	
+	"""Algorithm class for solving Sudokus using Peter Norvig algorithm"""
+	def __init__(self, matrixhandler):
+		"""Initializes a new Norvig algorithm instance.
+		
+		It calls the Algorithm initializator and additionally adds the
+		following instance attributes:
+		_squares -- ['A1', 'A2' ..., 'B1', 'B2', ... , 'I9']
+		_values -- {'A1': '123456789', ... 'I9': '123456789'}
+		_units -- {'A1': [[row], [column], [quadrant]], ... }
+		_peers -- {'A1': set(row, column, quadrant), ... }
+
+		"""
+		Algorithm.__init__(self, matrixhandler)
+		self._construct_default_data_structures()
+		self._construct_units_and_peers()
+
 	def solve(self):
 		"""
 		solve() -> matrixhandler
@@ -83,51 +77,90 @@ class Norvig(Algorithm):
 		else:
 			return None
 	
-	def _to_grid(self):
-		"""
-		_to_grid()
+	def _construct_default_data_structures(self):
+		"""Constructs default data structures used by Norvig algorithm.
 		
-		Convert input_matrix to a grid structure: {'A1':'<digit>', ...}
+		Two data structures are used:
+		_squares -- ['A1', 'A2' ..., 'B1', 'B2', ... , 'I9']
+		_values -- {'A1': '123456789', ... 'I9': '123456789'}
+
 		"""
-		chars = [str(c) for lines in self._input_matrix.first_matrix\
-															for c in lines]
+		self._squares = self._cross(self._rows, self._columns)
+		self._values = dict((s, self._columns) for s in self._squares)
+
+	def _construct_units_and_peers(self):
+		"""Constructs dictionaries containing each fields influence.
+		
+		Fields can be influenced by its row, column and quadrant it belongs.
+		Norvig will use two types of dictionaries for this purpose:
+		_units -- {'A1': [[row], [column], [quadrant]], ... }
+		_peers -- {'A1': set(row, column, quadrant), ... }
+
+		"""
+		unitlist = (
+			[self._cross(self._rows, c) for c in self._columns] +
+			[self._cross(r, self._columns) for r in self._rows] +
+			[
+				self._cross(rs, cs)
+					for rs in ('ABC','DEF','GHI')
+						for cs in ('123','456','789')
+			]
+		)
+		self._units = dict(
+			(s, [u for u in unitlist if s in u]) for s in self._squares
+		)
+		self._peers = dict(
+			(s, set(sum(self._units[s], [])) - set([s]))
+				for s in self._squares
+		)
+
+	def _to_grid(self):
+		"""Returns input_matrix as a grid dictionary.
+		
+		The structure is: {'A1':'<digit>', ...}
+
+		"""
+		chars = [
+			str(c) for lines in self._input_matrix.first_matrix
+						for c in lines
+		]
 		return dict(zip(self._squares, chars))
 	
 	def _to_matrix(self, grid):
-		"""
-		_to_matrix(grid) -> matrixhandler
-		
-		Convert given grid to a matrix and return it as a new matrixhandler.
-		"""
+		"""Returns a new matrix handler from grid converted to a matrix."""
 		matrix = []
 		for letter in self._rows:
-			matrix.append([int(grid[field]) for field in\
-									self._squares if field[0]==letter])
-		
+			matrix.append(
+				[
+					int(grid[field]) for field in self._squares
+										if field[0]==letter
+				]
+			)
 		return MatrixHandler(matrix)
 	
 	def _construct_possible_values_grid(self, grid):
-		"""
-		_construct_possible_values_grid(grid) -> values
+		"""Returns a dictionary grid with possible values for fields in grid.
 		
-		Use grid to build and return values with possible values for a field:
+		Dictionary grid will have the following format:
 		{'A1':'12', ... 'I9':'3469'}
+
 		"""
 		values = dict((field, self._digits) for field in self._squares)
 		for field, digit in grid.items():
-			if digit in self._digits and not self._assign(values, field, digit):
+			if digit in self._digits and (not
+			self._assign(values, field, digit)):
 				return False
 		return values
 	
 	def _assign(self, grid, field, digit):
-		"""
-		_assign(grid, field, digit) -> values
+		"""Returns values after finished removing other digits successfully.
 		
-		Use grid and attempt to eliminate all other digits in grid which result
-		in a violation until only the possible values for the field remain.
+		Use grid and attempt to eliminate all other digits in grid which
+		result in a violation until only the possible values for the field
+		remain.
 		
-		Return values after finished eliminating other digits successfully.
 		Return False if a contradiction has been found during elimination.
+		
 		"""
 		other_values = grid[field].replace(digit, '')
 		if all(self._eliminate(grid, field, odigt) for odigt in other_values):
@@ -136,28 +169,29 @@ class Norvig(Algorithm):
 			return False
 	
 	def _eliminate(self, grid, field, digit):
-		"""
-		_eliminate(grid, field, digit) -> values
+		"""Return values, same as grid, but with digit eliminated in field.
 		
 		Use grid and attempt to eliminate digit from field. Consider case:
 		(1) If field is reduced to one digit, eliminate this digit from peers
 		(2) If unit is reduced to only one place for digit, assign it there
 		
-		Return values, same as grid, but with digit eliminated in field
 		Return False if a contradiction has been found.
 		"""
 		if digit not in grid[field]:
-			return grid ## Already eliminated
+			return grid									# Already eliminated
 		grid[field] = grid[field].replace(digit,'')
 		
 		# (1) If field is reduced to one value val,
 		#     then eliminate val from the peers.
 		if len(grid[field]) == 0:
-			return False ## Contradiction: removed last value
+			return False 				# Contradiction: removed last value
 		elif len(grid[field]) == 1:
 			val = grid[field]
-			if not all(self._eliminate(grid, peer, val)\
-											for peer in self._peers[field]):
+			if not all(
+				self._eliminate(
+					grid, peer, val
+				) for peer in self._peers[field]
+			):
 				return False
 		
 		# (2) If unit is reduced to only one place for digit,
@@ -165,39 +199,39 @@ class Norvig(Algorithm):
 		for unit in self._units[field]:
 			dplaces = [field for field in unit if digit in grid[field]]
 			if len(dplaces) == 0:
-				return False ## Contradiction: no place for this value
+				return False		# Contradiction: no place for this value
 			elif len(dplaces) == 1:
-				# d can only be in one place in unit; assign it there
 				if not self._assign(grid, dplaces[0], digit):
 					return False
 		return grid
 	
 	def _search(self, values):
-		"""
-		_search(values) -> grid
+		"""Return grid if successfully reduced all fields to one digit.
 		
 		Search in values for fields with more than one digit, attempt to assign
 		arbitrarily one of the digits to that field, if no contradiction found.
 		
-		Repeat until each field has only one digit.
-		
-		Return grid if successfully reduced all fields to one digit (sudoku
-		has been solved).
+		Repeat until each field has only one digit (sudoku has been solved).
 		
 		Return False if contradiction found (sudoku is unsolvable).
+		
 		"""
 		if values is False:
-			return False ## Failed earlier
+			return False									# Failed earlier
 		if all(len(values[field]) == 1 for field in self._squares):
-			return values ## Solved!
+			return values											# Solved!
 		
-		## Chose the unfilled field with the fewest possibilities
-		npos, field = min((len(values[field]), field)\
-						for field in self._squares if len(values[field]) > 1)
+		npos, field = min(
+			(len(values[field]), field) for field in self._squares
+											if len(values[field]) > 1
+		)
 		
-		return self._some(self._search(self._assign(values.copy(),\
-									field, digit)) for digit in values[field])
-
+		return self._some(
+			self._search(
+				self._assign(values.copy(), field, digit)
+			) for digit in values[field]
+		)
+		
 
 class XAlgorithm(Algorithm):
 	
--- test_solver.py
+++ test_solver.py
@@ -1,19 +1,21 @@
 import unittest
 from itertools import product
+
 from solver import Algorithm, Norvig, XAlgorithm, Backtracking
 from validmatrix import MatrixHandler
 
 
 class TestAlgorithm(unittest.TestCase):
 	def setUp(self):
-		
 		# Values to test cross(A, B)
 		#---------------------------------------------------------------------
 		self.A = 'ABC'
 		self.B = '123'
-		self.crossAB = ['A1', 'A2', 'A3',\
-						'B1', 'B2', 'B3',\
-						'C1', 'C2', 'C3']
+		self.crossAB = [
+			'A1', 'A2', 'A3',
+			'B1', 'B2', 'B3',
+			'C1', 'C2', 'C3'
+		]
 		#---------------------------------------------------------------------
 		
 		# Values to test some(sequence)
@@ -22,14 +24,19 @@ class TestAlgorithm(unittest.TestCase):
 		self.sequence_negative = [''*20]
 		self.expected_element = self.A[0]
 		#---------------------------------------------------------------------
+	def test_solve_raise_implementation_error_if_called(self):
+		algorithm = Algorithm(MatrixHandler([]))
+		self.assertRaises(NotImplementedError, algorithm.solve)
+
 	def test_cross(self):
 		algorithm = Algorithm(MatrixHandler([]))
-		self.assertEqual(self.crossAB,algorithm._cross(self.A,self.B))
+		self.assertEqual(self.crossAB, algorithm._cross(self.A, self.B))
 
 	def test_some(self):
 		algorithm = Algorithm(MatrixHandler([]))
-		self.assertEqual(self.expected_element,\
-										algorithm._some(self.sequence))
+		self.assertEqual(
+			self.expected_element, algorithm._some(self.sequence)
+		)
 	
 	def test_some_negative(self):
 		algorithm = Algorithm(MatrixHandler([]))
@@ -37,79 +44,81 @@ class TestAlgorithm(unittest.TestCase):
 
 class TestNorvigAlgorithm(unittest.TestCase):
 	def setUp(self):
-	
 		# Values to test converting to grid
 		#---------------------------------------------------------------------
-		self.matrix = [ [4, 0, 0, 0, 0, 0, 8, 0, 5],\
-						[0, 3, 0, 0, 0, 0, 0, 0, 0],\
-						[0, 0, 0, 7, 0, 0, 0, 0, 0],\
-						[0, 2, 0, 0, 0, 0, 0, 6, 0],\
-						[0, 0, 0, 0, 8, 0, 4, 0, 0],\
-						[0, 0, 0, 0, 1, 0, 0, 0, 0],\
-						[0, 0, 0, 6, 0, 3, 0, 7, 0],\
-						[5, 0, 0, 2, 0, 0, 0, 0, 0],\
-						[1, 0, 4, 0, 0, 0, 0, 0, 0] ]
+		self.matrix = [
+			[4, 0, 0, 0, 0, 0, 8, 0, 5],
+			[0, 3, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 7, 0, 0, 0, 0, 0],
+			[0, 2, 0, 0, 0, 0, 0, 6, 0],
+			[0, 0, 0, 0, 8, 0, 4, 0, 0],
+			[0, 0, 0, 0, 1, 0, 0, 0, 0],
+			[0, 0, 0, 6, 0, 3, 0, 7, 0],
+			[5, 0, 0, 2, 0, 0, 0, 0, 0],
+			[1, 0, 4, 0, 0, 0, 0, 0, 0]
+		]
 		self.mtrxhandler = MatrixHandler(self.matrix)
 		
-		self.grid = {'A1':'4', 'A2':'0', 'A3':'0', 'A4':'0', 'A5':'0',\
-					 'A6':'0', 'A7':'8', 'A8':'0', 'A9':'5',\
-					 'B1':'0', 'B2':'3', 'B3':'0', 'B4':'0', 'B5':'0',\
-					 'B6':'0', 'B7':'0', 'B8':'0', 'B9':'0',\
-					 'C1':'0', 'C2':'0', 'C3':'0', 'C4':'7', 'C5':'0',\
-					 'C6':'0', 'C7':'0', 'C8':'0', 'C9':'0',\
-					 'D1':'0', 'D2':'2', 'D3':'0', 'D4':'0', 'D5':'0',\
-					 'D6':'0', 'D7':'0', 'D8':'6', 'D9':'0',\
-					 'E1':'0', 'E2':'0', 'E3':'0', 'E4':'0', 'E5':'8',\
-					 'E6':'0', 'E7':'4', 'E8':'0', 'E9':'0',\
-					 'F1':'0', 'F2':'0', 'F3':'0', 'F4':'0', 'F5':'1',\
-					 'F6':'0', 'F7':'0', 'F8':'0', 'F9':'0',\
-					 'G1':'0', 'G2':'0', 'G3':'0', 'G4':'6', 'G5':'0',\
-					 'G6':'3', 'G7':'0', 'G8':'7', 'G9':'0',\
-					 'H1':'5', 'H2':'0', 'H3':'0', 'H4':'2', 'H5':'0',\
-					 'H6':'0', 'H7':'0', 'H8':'0', 'H9':'0',\
-					 'I1':'1', 'I2':'0', 'I3':'4', 'I4':'0', 'I5':'0',
-					 'I6':'0', 'I7':'0', 'I8':'0', 'I9':'0'}
+		self.grid = {
+			'A1':'4', 'A2':'0', 'A3':'0', 'A4':'0', 'A5':'0',
+			'A6':'0', 'A7':'8', 'A8':'0', 'A9':'5',
+			
+			'B1':'0', 'B2':'3', 'B3':'0', 'B4':'0', 'B5':'0',
+			'B6':'0', 'B7':'0', 'B8':'0', 'B9':'0',
+			
+			'C1':'0', 'C2':'0', 'C3':'0', 'C4':'7', 'C5':'0',
+			'C6':'0', 'C7':'0', 'C8':'0', 'C9':'0',
+			
+			'D1':'0', 'D2':'2', 'D3':'0', 'D4':'0', 'D5':'0',
+			'D6':'0', 'D7':'0', 'D8':'6', 'D9':'0',
+			
+			'E1':'0', 'E2':'0', 'E3':'0', 'E4':'0', 'E5':'8',
+			'E6':'0', 'E7':'4', 'E8':'0', 'E9':'0',
+			
+			'F1':'0', 'F2':'0', 'F3':'0', 'F4':'0', 'F5':'1',
+			'F6':'0', 'F7':'0', 'F8':'0', 'F9':'0',
+			
+			'G1':'0', 'G2':'0', 'G3':'0', 'G4':'6', 'G5':'0',
+			'G6':'3', 'G7':'0', 'G8':'7', 'G9':'0',
+			
+			'H1':'5', 'H2':'0', 'H3':'0', 'H4':'2', 'H5':'0',
+			'H6':'0', 'H7':'0', 'H8':'0', 'H9':'0',
+			
+			'I1':'1', 'I2':'0', 'I3':'4', 'I4':'0', 'I5':'0',
+			'I6':'0', 'I7':'0', 'I8':'0', 'I9':'0'
+		}
 		#---------------------------------------------------------------------
 		
 		# Values to test construct_possible_values_grid
 		#---------------------------------------------------------------------
-		self.grid_possible_values = {'A1':'4', 'A2':'1679', 'A3':'12679',\
-									 'A4':'139', 'A5':'2369', 'A6':'269',\
-									 'A7':'8','A8':'1239', 'A9':'5',\
-									 #-----------------------------------------
-									 'B1':'26789', 'B2':'3', 'B3':'1256789',\
-									 'B4':'14589', 'B5':'24569',\
-									 'B6':'245689', 'B7':'12679',\
-									 'B8':'1249', 'B9':'124679',\
-									 #-----------------------------------------
-									 'C1':'2689', 'C2':'15689', 'C3':'125689',\
-									 'C4':'7', 'C5':'234569', 'C6':'245689',\
-									 'C7':'12369', 'C8':'12349',\
-									 'C9':'123469',\
-									 #-----------------------------------------
-									 'D1':'3789', 'D2':'2', 'D3':'15789',\
-									 'D4':'3459', 'D5':'34579', 'D6':'4579',\
-									 'D7':'13579', 'D8':'6', 'D9':'13789',\
-									 #-----------------------------------------
-									 'E1':'3679', 'E2':'15679', 'E3':'15679',\
-									 'E4':'359', 'E5':'8', 'E6':'25679',\
-									 'E7':'4', 'E8':'12359', 'E9':'12379',\
-									 #-----------------------------------------
-									 'F1':'36789','F2':'4', 'F3':'56789',\
-									 'F4':'359', 'F5':'1', 'F6':'25679',\
-									 'F7':'23579', 'F8':'23589', 'F9':'23789',\
-									 #-----------------------------------------
-									 'G1':'289','G2':'89','G3':'289','G4':'6',\
-									 'G5':'459','G6':'3','G7':'1259','G8':'7',\
-									 'G9':'12489',\
-									 #-----------------------------------------
-									 'H1':'5', 'H2':'6789', 'H3':'3',\
-									 'H4':'2', 'H5':'479', 'H6':'1',\
-									 'H7':'69', 'H8':'489', 'H9':'4689',\
-									 #-----------------------------------------
-									 'I1':'1', 'I2':'6789', 'I3':'4',\
-									 'I4':'589', 'I5':'579', 'I6':'5789',\
-									 'I7':'23569', 'I8':'23589', 'I9':'23689'}
+		self.grid_possible_values = {
+			'A1':'4', 'A2':'1679', 'A3':'12679', 'A4':'139', 'A5':'2369',
+			'A6':'269', 'A7':'8','A8':'1239', 'A9':'5',
+			
+			'B1':'26789', 'B2':'3', 'B3':'1256789', 'B4':'14589', 'B5':'24569',
+			'B6':'245689', 'B7':'12679','B8':'1249', 'B9':'124679',
+			
+			'C1':'2689', 'C2':'15689', 'C3':'125689','C4':'7', 'C5':'234569',
+			'C6':'245689', 'C7':'12369', 'C8':'12349', 'C9':'123469',
+			
+			'D1':'3789', 'D2':'2', 'D3':'15789', 'D4':'3459', 'D5':'34579',
+			'D6':'4579', 'D7':'13579', 'D8':'6', 'D9':'13789',
+			
+			'E1':'3679', 'E2':'15679', 'E3':'15679', 'E4':'359', 'E5':'8',
+			'E6':'25679', 'E7':'4', 'E8':'12359', 'E9':'12379',
+			
+			'F1':'36789','F2':'4', 'F3':'56789', 'F4':'359', 'F5':'1',
+			'F6':'25679', 'F7':'23579', 'F8':'23589', 'F9':'23789',
+			
+			'G1':'289','G2':'89','G3':'289','G4':'6', 'G5':'459',
+			'G6':'3','G7':'1259','G8':'7', 'G9':'12489',
+			
+			'H1':'5', 'H2':'6789', 'H3':'3', 'H4':'2', 'H5':'479',
+			'H6':'1', 'H7':'69', 'H8':'489', 'H9':'4689',
+			
+			'I1':'1', 'I2':'6789', 'I3':'4', 'I4':'589', 'I5':'579',
+			'I6':'5789', 'I7':'23569', 'I8':'23589', 'I9':'23689'
+		}
 		#---------------------------------------------------------------------
 		
 		# Values to test assign and eliminate
@@ -122,56 +131,72 @@ class TestNorvigAlgorithm(unittest.TestCase):
 		
 		# Values to test search and solve
 		#---------------------------------------------------------------------
-		self.matrix_multiple = [[0, 8, 0, 0, 0, 9, 7, 4, 3],\
-								[0, 5, 0, 0, 0, 8, 0, 1, 0],\
-								[0, 1, 0, 0, 0, 0, 0, 0, 0],\
-								[8, 0, 0, 0, 0, 5, 0, 6, 0],\
-								[0, 0, 0, 8, 0, 4, 0, 0, 0],\
-								[0, 0, 0, 3, 0, 0, 0, 0, 6],\
-								[0, 0, 0, 0, 0, 0, 0, 7, 0],\
-								[0, 3, 0, 5, 0, 0, 0, 8, 0],\
-								[9, 7, 2, 4, 0, 0, 0, 5, 0]]
+		self.matrix_multiple = [
+			[0, 8, 0, 0, 0, 9, 7, 4, 3],
+			[0, 5, 0, 0, 0, 8, 0, 1, 0],
+			[0, 1, 0, 0, 0, 0, 0, 0, 0],
+			[8, 0, 0, 0, 0, 5, 0, 6, 0],
+			[0, 0, 0, 8, 0, 4, 0, 0, 0],
+			[0, 0, 0, 3, 0, 0, 0, 0, 6],
+			[0, 0, 0, 0, 0, 0, 0, 7, 0],
+			[0, 3, 0, 5, 0, 0, 0, 8, 0],
+			[9, 7, 2, 4, 0, 0, 0, 5, 0]
+		]
 		self.mtrxhandler_multiple = MatrixHandler(self.matrix_multiple)
 		
-		self.matrix_unsolvable = [ [1, 2, 3, 4, 5, 6, 7, 8, 0],\
-									[0, 0, 0, 0, 0, 0, 0, 0, 2],\
-									[0, 9, 0, 1, 0, 0, 0, 0, 3],\
-									[0, 0, 0, 0, 0, 0, 0, 0, 4],\
-									[0, 0, 0, 0, 7, 0, 0, 0, 5],\
-									[0, 0, 0, 0, 0, 0, 0, 0, 6],\
-									[0, 0, 0, 0, 1, 0, 0, 0, 7],\
-									[0, 0, 1, 0, 0, 2, 0, 0, 8],\
-									[0, 0, 0, 0, 0, 0, 0, 0, 9] ]
+		self.matrix_unsolvable = [
+			[1, 2, 3, 4, 5, 6, 7, 8, 0],\
+			[0, 0, 0, 0, 0, 0, 0, 0, 2],\
+			[0, 9, 0, 1, 0, 0, 0, 0, 3],\
+			[0, 0, 0, 0, 0, 0, 0, 0, 4],\
+			[0, 0, 0, 0, 7, 0, 0, 0, 5],\
+			[0, 0, 0, 0, 0, 0, 0, 0, 6],\
+			[0, 0, 0, 0, 1, 0, 0, 0, 7],\
+			[0, 0, 1, 0, 0, 2, 0, 0, 8],\
+			[0, 0, 0, 0, 0, 0, 0, 0, 9]
+		]
 		self.mtrxhandler_unsolvable = MatrixHandler(self.matrix_unsolvable)
 		
-		self.grid_solved = {'A1':'4', 'A2':'1', 'A3':'7', 'A4':'3', 'A5':'6',\
-							'A6':'9', 'A7':'8', 'A8':'2', 'A9':'5',\
-							'B1':'6', 'B2':'3', 'B3':'2', 'B4':'1', 'B5':'5',\
-							'B6':'8', 'B7':'9', 'B8':'4', 'B9':'7',\
-							'C1':'9', 'C2':'5', 'C3':'8', 'C4':'7', 'C5':'2',\
-							'C6':'4', 'C7':'3', 'C8':'1', 'C9':'6',\
-							'D1':'8', 'D2':'2', 'D3':'5', 'D4':'4', 'D5':'3',\
-							'D6':'7', 'D7':'1', 'D8':'6', 'D9':'9',\
-							'E1':'7', 'E2':'9', 'E3':'1', 'E4':'5', 'E5':'8',\
-							'E6':'6', 'E7':'4', 'E8':'3', 'E9':'2',\
-							'F1':'3', 'F2':'4', 'F3':'6', 'F4':'9', 'F5':'1',\
-							'F6':'2', 'F7':'7', 'F8':'5', 'F9':'8',\
-							'G1':'2', 'G2':'8', 'G3':'9', 'G4':'6', 'G5':'4',\
-							'G6':'3', 'G7':'5', 'G8':'7', 'G9':'1',\
-							'H1':'5', 'H2':'7', 'H3':'3', 'H4':'2', 'H5':'9',\
-							'H6':'1', 'H7':'6', 'H8':'8', 'H9':'4',\
-							'I1':'1', 'I2':'6', 'I3':'4', 'I4':'8', 'I5':'7',\
-							'I6':'5', 'I7':'2', 'I8':'9', 'I9':'3'}		
+		self.grid_solved = {
+			'A1':'4', 'A2':'1', 'A3':'7', 'A4':'3', 'A5':'6',
+			'A6':'9', 'A7':'8', 'A8':'2', 'A9':'5',
+			
+			'B1':'6', 'B2':'3', 'B3':'2', 'B4':'1', 'B5':'5',
+			'B6':'8', 'B7':'9', 'B8':'4', 'B9':'7',
+			
+			'C1':'9', 'C2':'5', 'C3':'8', 'C4':'7', 'C5':'2',
+			'C6':'4', 'C7':'3', 'C8':'1', 'C9':'6',
+			
+			'D1':'8', 'D2':'2', 'D3':'5', 'D4':'4', 'D5':'3',
+			'D6':'7', 'D7':'1', 'D8':'6', 'D9':'9',
+			
+			'E1':'7', 'E2':'9', 'E3':'1', 'E4':'5', 'E5':'8',
+			'E6':'6', 'E7':'4', 'E8':'3', 'E9':'2',
+			
+			'F1':'3', 'F2':'4', 'F3':'6', 'F4':'9', 'F5':'1',
+			'F6':'2', 'F7':'7', 'F8':'5', 'F9':'8',
+			
+			'G1':'2', 'G2':'8', 'G3':'9', 'G4':'6', 'G5':'4',
+			'G6':'3', 'G7':'5', 'G8':'7', 'G9':'1',
+			
+			'H1':'5', 'H2':'7', 'H3':'3', 'H4':'2', 'H5':'9',
+			'H6':'1', 'H7':'6', 'H8':'8', 'H9':'4',
+			
+			'I1':'1', 'I2':'6', 'I3':'4', 'I4':'8', 'I5':'7',
+			'I6':'5', 'I7':'2', 'I8':'9', 'I9':'3'
+		}		
 
-		self.matrix_solved = [ [4, 1, 7, 3, 6, 9, 8, 2, 5],\
-								[6, 3, 2, 1, 5, 8, 9, 4, 7],\
-								[9, 5, 8, 7, 2, 4, 3, 1, 6],\
-								[8, 2, 5, 4, 3, 7, 1, 6, 9],\
-								[7, 9, 1, 5, 8, 6, 4, 3, 2],\
-								[3, 4, 6, 9, 1, 2, 7, 5, 8],\
-								[2, 8, 9, 6, 4, 3, 5, 7, 1],\
-								[5, 7, 3, 2, 9, 1, 6, 8, 4],\
-								[1, 6, 4, 8, 7, 5, 2, 9, 3] ]	
+		self.matrix_solved = [
+			[4, 1, 7, 3, 6, 9, 8, 2, 5],
+			[6, 3, 2, 1, 5, 8, 9, 4, 7],
+			[9, 5, 8, 7, 2, 4, 3, 1, 6],
+			[8, 2, 5, 4, 3, 7, 1, 6, 9],
+			[7, 9, 1, 5, 8, 6, 4, 3, 2],
+			[3, 4, 6, 9, 1, 2, 7, 5, 8],
+			[2, 8, 9, 6, 4, 3, 5, 7, 1],
+			[5, 7, 3, 2, 9, 1, 6, 8, 4],
+			[1, 6, 4, 8, 7, 5, 2, 9, 3]
+		]
 		self.mtrxhandler_solved = MatrixHandler(self.matrix_solved)
 		#---------------------------------------------------------------------
 		
@@ -181,41 +206,61 @@ class TestNorvigAlgorithm(unittest.TestCase):
 	
 	def test_construct_possible_values_grid(self):
 		norvig = Norvig(self.mtrxhandler)
-		self.assertEqual(self.grid_possible_values,\
-							norvig._construct_possible_values_grid(self.grid))
+		self.assertEqual(
+			self.grid_possible_values,
+			norvig._construct_possible_values_grid(self.grid)
+		)
 	
 	def test_eliminate_false_if_cotradiction_met(self):
 		norvig = Norvig(self.mtrxhandler)
-		self.assertFalse(norvig._eliminate(self.grid_contradiction,\
-								self.field, self.value_with_contradiction))
+		self.assertFalse(
+			norvig._eliminate(
+				self.grid_contradiction,
+				self.field, self.value_with_contradiction
+			)
+		)
 	
 	def test_eliminate_value_if_valid(self):
 		norvig = Norvig(self.mtrxhandler)
-		actual = norvig._eliminate(self.grid_possible_values, self.field,\
-							self.value_valid)[self.field]
+		actual = norvig._eliminate(
+			self.grid_possible_values,
+			self.field,
+			self.value_valid
+		)[self.field]
 		self.assertTrue(self.value_valid not in actual)
 
 	def test_assign_false_if_cotradiction_met(self):
 		norvig = Norvig(self.mtrxhandler)
-		self.assertFalse(norvig._assign(self.grid, self.field,\
-							self.value_with_contradiction))
+		self.assertFalse(
+			norvig._assign(
+				self.grid, self.field,
+				self.value_with_contradiction
+			)
+		)
 	
 	def test_assign_value_if_valid(self):
 		norvig = Norvig(self.mtrxhandler)
 		expected = self.value_valid
-		actual = norvig._assign(self.grid_possible_values, self.field,\
-							self.value_valid)[self.field]
+		actual = norvig._assign(
+			self.grid_possible_values,
+			self.field,
+			self.value_valid
+		)[self.field]
 		self.assertEqual(expected, actual)
 	
 	def test_search(self):
 		norvig = Norvig(self.mtrxhandler)
-		self.assertEqual(self.grid_solved,\
-							norvig._search(self.grid_possible_values))
+		self.assertEqual(
+			self.grid_solved,
+			norvig._search(self.grid_possible_values)
+		)
 	
 	def test_solve(self):
 		norvig = Norvig(self.mtrxhandler)
-		self.assertEqual(self.mtrxhandler_solved.first_matrix,\
-							norvig.solve().first_matrix)
+		self.assertEqual(
+			self.mtrxhandler_solved.first_matrix,
+			norvig.solve().first_matrix
+		)
 
 	def test_solve_unsolvable_casemultiple(self):
 		norvig = Norvig(self.mtrxhandler_multiple)
@@ -224,6 +269,7 @@ class TestNorvigAlgorithm(unittest.TestCase):
 	def test_solve_unsolvable_caseunsolvable(self):
 		norvig = Norvig(self.mtrxhandler_unsolvable)
 		self.assertEqual(None, norvig.solve())
+
 
 class TestXAlgorithm(unittest.TestCase):
 	def setUp(self):

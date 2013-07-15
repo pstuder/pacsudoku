0a1d36ad487b9f78f425a64c05c73dd0622a66a3
adorado <arieldorado@gmail.com>
2013-07-15 11:40:13 -0400

    Configfile
    PEP8 and PEP257

--- source/config.py
+++ source/config.py
@@ -1,34 +1,54 @@
 class Configfile():
     supported_input_type_list = ["TXT", "CSV"]
     supported_output_type_list = ["Console", "File"]
-    supported_default_algorithm_list = ["Backtracking", "Norvig", "XAlgorithm"]
+    supported_default_algorithm_list = [
+                                "Backtracking", 
+                                "Norvig", "XAlgorithm"
+                                ]
     supported_difficulty_level_list = ["High","Medium", "Low"]
 
-    def __init__(self,input_type="TXT",output_type="Console",default_algorithm="Backtracking",difficulty_level="Low"):
+    def __init__(
+                 self,input_type="TXT",output_type="Console",
+                 default_algorithm="Backtracking",difficulty_level="Low"):
         """ This is a constructor of a Configfile class.
         
             The following instance attributes are created:
-            input_type -- Define the default file input type. If it is not defined, the  default value is TXT.
-            output_type -- Define the default output type. If it is not defined, the  default value is Console.
-            default_algorithm -- Define the default algorithm that will be used to solve a SUDOKU. If it is not defined, the  default value is Backtracking.
-            difficulty_level -- Define the difficult level of a SUDOKU game. If it is not defined, the  default value is Low.
+            input_type -- Define the default file input type. 
+                          If it is not defined, 
+                          the  default value is TXT.
+            output_type -- Define the default output type. 
+                           If it is not defined, 
+                           the  default value is Console.
+            default_algorithm -- Define the default algorithm that 
+                                 will be used to solve 
+                                 a SUDOKU. If it is not defined, 
+                                 the  default value is Backtracking.
+            difficulty_level -- Define the difficult level of a SUDOKU game. 
+                                If it is not defined, 
+                                the  default value is Low.
         """
-        if self.validate_input_type(input_type,self.supported_input_type_list)==True:
+        if self.validate_input_type(
+                        input_type, self.supported_input_type_list)==True:
             self.input_type=input_type
         else:
             self.input_type= "TXT"
         
-        if self.validate_input_type(output_type,self.supported_output_type_list)==True:
+        if self.validate_input_type(
+                        output_type,self.supported_output_type_list)==True:
             self.output_type=output_type
         else:
             self.output_type="Console"
             
-        if self.validate_input_type(default_algorithm,self.supported_default_algorithm_list)==True:    
+        if self.validate_input_type(
+                        default_algorithm,
+                        self.supported_default_algorithm_list)==True:    
             self.default_algorithm=default_algorithm
         else:
             self.default_algorithm="Backtracking"
             
-        if self.validate_input_type(difficulty_level,self.supported_difficulty_level_list)==True:
+        if self.validate_input_type(
+                        difficulty_level,
+                        self.supported_difficulty_level_list)==True:
             self.difficulty_level=difficulty_level
         else:
             self.difficulty_level="Low"

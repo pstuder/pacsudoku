80a62dc6f9c482cef502bd67b7caf205504dee55
adorado <arieldorado@gmail.com>
2013-07-15 10:34:33 -0400

    Configfile
    PEP8 and PEP257

--- source/config.py
+++ source/config.py
@@ -1,37 +1,52 @@
 class Configfile():
-    supported_inputTypes = ["TXT", "CSV"]
-    supported_outputTypes = ["Console", "File"]
-    supported_defaultAlgorithms = ["Backtracking", "Norvig", "XAlgorithm"]
-    supported_difficultyLevels = ["High","Medium", "Low"]
+    supported_input_type_list = ["TXT", "CSV"]
+    supported_output_type_list = ["Console", "File"]
+    supported_default_algorithm_list = ["Backtracking", "Norvig", "XAlgorithm"]
+    supported_difficulty_level_list = ["High","Medium", "Low"]
 
-    def __init__(self,inputType="TXT",outputType="Console",defaultAlgorithm="Backtracking",difficultyLevel="Low"):
-        """ This is a constructor of a Configfile class."""
-        if self.validateInputType(inputType,self.supported_inputTypes)==True:
-            self.inputType=inputType
-        else:
-            self.inputType= "TXT"
+    def __init__(self,input_type="TXT",output_type="Console",default_algorithm="Backtracking",difficulty_level="Low"):
+        """ This is a constructor of a Configfile class.
         
-        if self.validateInputType(outputType,self.supported_outputTypes)==True:
-            self.outputType=outputType
+            The following instance attributes are created:
+            input_type -- Define the default file input type. If it is not defined, the  default value is TXT.
+            output_type -- Define the default output type. If it is not defined, the  default value is Console.
+            default_algorithm -- Define the default algorithm that will be used to solve a SUDOKU. If it is not defined, the  default value is Backtracking.
+            difficulty_level -- Define the difficult level of a SUDOKU game. If it is not defined, the  default value is Low.
+        """
+        if self.validate_input_type(input_type,self.supported_input_type_list)==True:
+            self.input_type=input_type
         else:
-            self.outputType="Console"
-            
-        if self.validateInputType(defaultAlgorithm,self.supported_defaultAlgorithms)==True:    
-            self.defaultAlgorithm=defaultAlgorithm
-        else:
-            self.defaultAlgorithm="Backtracking"
-            
-        if self.validateInputType(difficultyLevel,self.supported_difficultyLevels)==True:
-            self.difficultyLevel=difficultyLevel
-        else:
-            self.difficultyLevel="Low"
+            self.input_type= "TXT"
         
-    def validateInputType(self,inputtype, formats):
+        if self.validate_input_type(output_type,self.supported_output_type_list)==True:
+            self.output_type=output_type
+        else:
+            self.output_type="Console"
+            
+        if self.validate_input_type(default_algorithm,self.supported_default_algorithm_list)==True:    
+            self.default_algorithm=default_algorithm
+        else:
+            self.default_algorithm="Backtracking"
+            
+        if self.validate_input_type(difficulty_level,self.supported_difficulty_level_list)==True:
+            self.difficulty_level=difficulty_level
+        else:
+            self.difficulty_level="Low"
+        
+    def validate_input_type(self,input_type, format_list):
+        """
+        Validate each input of a Configfile class
+        
+        The defined parameters means:
+        input_type -- Define the parameter input type
+        format_list -- List of possible values for each type.
+        """
         status=False
-        for a in range(len(formats)):
-            if inputtype==formats[a]:
+        for a in range(len(format_list)):
+            if input_type==format_list[a]:
                 status=True
                 break 
             else:
                 status=False
         return status
+
--- tests/testConfig.py
+++ tests/testConfig.py
@@ -9,48 +9,48 @@ class test_configfile(unittest.TestCase):
         self.assertTrue(self.default_config_file) 
     
     def test_default_inputType_is_TXT(self):
-        self.assertEqual(self.default_config_file.inputType,"TXT")             
+        self.assertEqual(self.default_config_file.input_type,"TXT")             
 
     def test_default_outputType_is_Console(self):
-        self.assertEqual(self.default_config_file.outputType,"Console")             
+        self.assertEqual(self.default_config_file.output_type,"Console")             
     
     def test_default_defaultAlgorithm_is_Backtracking(self):
-        self.assertEqual(self.default_config_file.defaultAlgorithm,"Backtracking")
+        self.assertEqual(self.default_config_file.default_algorithm,"Backtracking")
     
     def test_default_difficultyLevel_is_LOW(self):
-        self.assertEqual(self.default_config_file.difficultyLevel,"Low")
+        self.assertEqual(self.default_config_file.difficulty_level,"Low")
     
     def test_load_config_file_with_input_type_CSV(self):
         personal_configfile=Configfile("CSV", "Console","Backtracking","Low")
-        self.assertEqual(personal_configfile.inputType,"CSV")
+        self.assertEqual(personal_configfile.input_type,"CSV")
     
     def test_load_config_file_with_output_type_File(self):
         personal_configfile=Configfile("TXT", "File","Backtracking","Low")
-        self.assertEqual(personal_configfile.outputType,"File")    
+        self.assertEqual(personal_configfile.output_type,"File")    
 
     def test_load_config_file_with_Default_Algorithm_Peter_Novigs(self):
         personal_configfile=Configfile("TXT", "Console","Norvig","Low")
-        self.assertEqual(personal_configfile.defaultAlgorithm,"Norvig")         
+        self.assertEqual(personal_configfile.default_algorithm,"Norvig")         
 
     def test_load_config_file_with_difficultyLevel_Medium(self):
         personal_configfile=Configfile("TXT", "Console","Backtracking","Medium")
-        self.assertEqual(personal_configfile.difficultyLevel,"Medium")   
+        self.assertEqual(personal_configfile.difficulty_level,"Medium")   
     
     def test_load_config_file_does_not_allow_values_distinct_to_TXT_and_CSV(self):
         personal_configfile=Configfile("DOC", "Console","Backtracking","Low")
-        self.assertEqual(personal_configfile.inputType,"TXT")         
+        self.assertEqual(personal_configfile.input_type,"TXT")         
     
     def test_load_config_file_does_not_allow_values_distinct_to_Console_and_File(self):
         personal_configfile=Configfile("TXT", "Printer","Backtracking","Low")
-        self.assertEqual(personal_configfile.outputType,"Console")        
+        self.assertEqual(personal_configfile.output_type,"Console")        
     
     def test_load_config_file_does_not_allow_values_distinct_to_Backtracking_Peter_Novigs_or_Other_values(self):
         personal_configfile=Configfile("TXT", "Console","MyAlgorithm","Low")
-        self.assertEqual(personal_configfile.defaultAlgorithm,"Backtracking")
+        self.assertEqual(personal_configfile.default_algorithm,"Backtracking")
 
     def test_load_config_file_does_not_allow_values_distinct_to_Difficult_level_just_allow_High_Medium_Low_values(self):
         personal_configfile=Configfile("TXT", "Console","Backtracking","Hard")
-        self.assertEqual(personal_configfile.difficultyLevel,"Low")
+        self.assertEqual(personal_configfile.difficulty_level,"Low")
                       
 if __name__ == "__main__":
     unittest.main() 

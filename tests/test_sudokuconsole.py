1c1e3a7fc96a93cd756e37cb2831b95a36db2c24
adorado <arieldorado@gmail.com>
2013-07-24 12:11:54 -0400

    Create Console Class
    Fix the problem related to inexistent recovered memory positions

--- tests/test_sudokuconsole.py
+++ tests/test_sudokuconsole.py
@@ -31,7 +31,7 @@ class TestSudokuConsoleUserInterface(unittest.TestCase):
 
         self.expected_tuple_from_default_file = ("TXT", "Console",\
                                         "Backtracking", "Low")
-        
+        self.interactive_console=SudokuConsoleUserInterface(self.default_file_handler_xml)
         
         
     def test_default_game_should_print_default_config_values_in_Console(self):
@@ -43,10 +43,52 @@ class TestSudokuConsoleUserInterface(unittest.TestCase):
             default_game_selected.config.difficultyLevel)
         self.assertEqual(self.expected_tuple_from_default_file, defaul_tuple_from_file)
     
+    def test_that_the_mesage_is_dislayed_with_massege_format(self):
+        input_mesage="Example message"
+        self.assertEqual(mesage(input_mesage), self.interactive_console.mesage(input_mesage))
+
+    def test_that_the_mesage_is_dislayed_with_header_format(self):
+        input_mesage="Example message"
+        self.assertEqual(mesage(input_mesage), self.interactive_console.header(input_mesage))
+        
+    def test_that_the_return_h_m_s_return_the_current_time_as_float(self):
+        current_time,h,m,s=self.interactive_console.return_h_m_s()
+        self.assertEqual(type(current_time), float)
+        
+    def test_that_the_return_h_m_s_return_the_seconds_lower_or_equal_to_60(self):
+        current_time,h,m,s=self.interactive_console.return_h_m_s()
+        self.assertTrue(s<=60)
+        
+    def test_that_the_return_h_m_s_return_the_minutes_lower_or_equal_to_60(self):
+        current_time,h,m,s=self.interactive_console.return_h_m_s()
+        self.assertTrue(m<=60)
+            
+    def test_that_the_return_h_m_s_return_the_hours_type_is_float(self):
+        current_time,h,m,s=self.interactive_console.return_h_m_s()
+        self.assertEqual(type(h),float)
+    
+    def test_that_the_return_h_m_s_return_the_hours_type_is_float_when_is_loaded_is_true(self):
+        self.interactive_console.loaded_game==True
+        current_time,h,m,s=self.interactive_console.return_h_m_s()
+        self.assertEqual(type(h),float)
+
+
     def test_option_1_return_default_configuration(self):
         default_game_selected = SudokuConsoleUserInterface(self.default_file_handler_xml)
-        self.assertEqual('1', '1', default_game_selected.sudokumenu(""))
-        
+        self.assertIsInstance(default_game_selected,SudokuConsoleUserInterface)
+
+    #===========================================================================
+    # def test_option_1_return_default_configuration(self):
+    #     default_game_selected = SudokuConsoleUserInterface(self.default_file_handler_xml)
+    #     self.assertEqual('1', '1', default_game_selected.sudokumenu(""))
+    #===========================================================================
+
+    
+def mesage(mesage):
+    print "**************************************"
+    print mesage
+    print "**************************************"
+    
         
 if __name__ == "__main__":
     unittest.main()
